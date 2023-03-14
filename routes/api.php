f9d051fb8122a2ffb0aee140ad080af44d5044ae
@tsabit <tsabitimanana55@gmail.com>
2023-03-14 18:24:26 +0700

    Final Commit kayaknya

--- routes/api.php
+++ routes/api.php
@@ -29,6 +29,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::group(['middleware' => ['jwt.verify']], function(){
     Route::group(['middleware' => ['api.receptionist']], function(){
 
+        Route::put('/orders/status/{id}', [OrderController::class, 'upstatus']); //update status
+        Route::post('/orderfilter', [OrderController::class, 'orderFilter']);
+        Route::get('/orders', [OrderController::class, 'show']);
+        Route::get('/orders/{id}', [OrderController::class, 'detail']);
+        Route::put('/orders/{id}', [OrderController::class,'store']); //TIDAK PERLU
+
     });
     Route::group(['middleware' => ['api.admin']], function(){
         // CRUD user
@@ -36,46 +42,40 @@ Route::group(['middleware' => ['jwt.verify']], function(){
         Route::put('/user/{id}', [UserController::class, 'update']);
         Route::delete('/user/{id}', [UserController::class, 'delete']);
         Route::get('/user', [UserController::class, 'show']);
-        
-        // CRUD type
+
+            // CRUD type
         Route::post('/type',[TypeController::class,'store']);
         Route::delete('/type/{id}',[TypeController::class,'destroy']);
         Route::post('/type/{id}', [TypeController::class,'update']);
+
+
+        // CRUD room
+        Route::get('/room',[RoomController::class,'show']); 
+        Route::get('/room/{id}',[RoomController::class,'detail']); 
+        Route::post('/room',[RoomController::class,'store']);
+        Route::delete('/room/{id}',[RoomController::class,'destroy']);
+        Route::put('/room/{id}', [RoomController::class,'update']);
     });
     
 });
 
-//CRUD type
-Route::get('/type',[TypeController::class,'show']);
-Route::get('/type/{id}',[TypeController::class,'detail']);
-Route::get('/type/detail/{type_id}',[TypeController::class,'detailType']);
 
-// CRUD room
-Route::get('/room',[RoomController::class,'show']); 
-Route::get('/room/{id}',[RoomController::class,'detail']); 
-Route::post('/room',[RoomController::class,'store']);
-Route::delete('/room/{id}',[RoomController::class,'destroy']);
-Route::put('/room/{id}', [RoomController::class,'update']);
+    Route::get('/type',[TypeController::class,'show']);
+    Route::get('/type/{id}',[TypeController::class,'detail']);
+    Route::get('/type/detail/{type_id}',[TypeController::class,'detailType']); // TIDAK PERLU
+
 
 //Orders
 Route::post('/orders', [OrderController::class,'create']); //bikin order
-Route::get('/orders', [OrderController::class, 'show']);
-Route::get('/orders/{id}', [OrderController::class, 'detail']);
-Route::put('/orders/status/{id}', [OrderController::class, 'upstatus']); //update status
-Route::put('/order/{id}', [OrderController::class,'store']);
-Route::post('/orderFilter', [OrderController::class, 'orderFilter']);
-
-//Orders Detail
-// Route::post('/detailorder',[OrdersDetailController::class, 'index']);
 
 //date filter
 Route::post('/datefilter',[OrdersDetailController::class, 'index']);
 
 //check order
-Route::post('/checkorder',[OrdersDetailController::class, 'checkorder']);
+// Route::post('/checkorder',[OrdersDetailController::class, 'checkorder']);
 
 //reciept
-Route::get('/orders/receipt/{order_number}',[OrdersDetailController::class, 'reciept']);
+Route::get('/receipt/{order_number}',[OrdersDetailController::class, 'reciept']);
 
 
 Route::post('/login', [UserController::class, 'login']);
